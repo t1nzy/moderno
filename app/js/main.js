c9ff7943ba99e822c82a3902703d926a8762a57a
T1nzy <dimaret87@gmail.com>
2020-11-10 18:13:01 +0200

    added advantages

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
-$(function(){
-    var mixer = mixitup('.products__inner-box');
+$(function () {
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "15px",
+    readOnly: true,
+  });
+
+  var mixer = mixitup(".products__inner-box");
+});
+
+$(function () {
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow:4,
+    slidesToScroll:4
+  });
 });
